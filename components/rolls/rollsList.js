936f94330eb564df06db1af057df601f58b1ca04
joemanf <joemanf0@gmail.com>
2024-04-28 01:35:06 -0400

    Added plus minus checks if no spaces exist

--- components/rolls/rollsList.js
+++ components/rolls/rollsList.js
@@ -9,7 +9,7 @@ export default function RollsList({ rolls, loadMoreRolls, name }) {
         <div key={`${roll.data.time}${roll.data.name}`} className='flex border p-1 items-center'>
           <p className='min-w-fit'>{roll.data.time}</p>
           <p className='pl-2 border-l mx-2 min-h-full'>
-            <span className={name === roll.data.name ? 'text-blue-300' : ''} >{roll.data.name}</span> rolled: {roll.data.logic}. Results: <span>{roll.data.result}</span> <span className='font-bold'>Total:</span> <span className={(roll.data.logic.split(' ')[0].trim() === '1d20' || roll.data.logic.split(' ')[0].trim() === 'd20') && parseInt(roll.data.total) >= 20 ? 'text-blue-300' : ''}>{roll.data.total}</span>
+            <span className={name === roll.data.name ? 'text-blue-300' : ''} >{roll.data.name}</span> rolled: {roll.data.logic}. Results: <span>{roll.data.result}</span> <span className='font-bold'>Total:</span> <span className={(roll.data.logic.split(' ')[0].trim() === '1d20' || roll.data.logic.split(' ')[0].trim() === 'd20' || roll.data.logic.split('+')[0].trim() === '1d20' || roll.data.logic.split('+')[0].trim() === 'd20' || roll.data.logic.split('-')[0].trim() === '1d20' || roll.data.logic.split('-')[0].trim() === 'd20') && parseInt(roll.data.total) >= 20 ? 'text-blue-300' : ''}>{roll.data.total}</span>
           </p>
         </div>
       )) : <></>}
